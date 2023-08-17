322c947384c22dc4c4e57471c3f962af7761e34f
mominalix <mominalix@gmail.com>
2023-08-18 01:17:15 +0500

    new file:   Particle Collision Simulation/Readme.md 	new file:   Particle Collision Simulation/partilce_simulation.py

---
+++ Particle Collision Simulation/Readme.md
@@ -0,0 +1,37 @@
+# Particle Physics Simulation with Gravitational Effects
+
+This repository contains a simple particle physics simulation that demonstrates the behavior of particles with gravitational effects. The simulation is implemented using Pygame for visualization and PyOpenGL for rendering.
+
+## Prerequisites
+
+- Python 3.x
+- Pygame (install using `pip install pygame`)
+- PyOpenGL (install using `pip install PyOpenGL`)
+
+## How to Run
+
+1. Clone the repository to your local machine using Git or download the ZIP file.
+
+2. Open a terminal or command prompt and navigate to the repository's directory.
+
+3. Run the simulation script using the following command:
+
+    ```
+    python particle_simulation.py
+    ```
+
+4. The simulation window will open, and you'll see particles moving and interacting with gravitational effects.
+
+5. To exit the simulation, simply close the window.
+
+## Functionality
+
+The simulation showcases the following functionalities:
+
+- Particles: A number of particles are generated randomly on the screen, each with a random weight, initial position, and velocity.
+
+- Gravitational Effects: Particles experience a simplified form of gravitational interaction. As particles cross each other, their velocities and directions are affected by the gravitational pull based on their weights.
+
+- Bouncing off Borders: The particles bounce off the borders of the simulation window, creating a bouncing effect when they reach the edges.
+
+
---
+++ Particle Collision Simulation/partilce_simulation.py
@@ -0,0 +1,76 @@
+import pygame
+import numpy as np
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+
+# Particle class to store particle properties
+class Particle:
+    def __init__(self, x, y, vx, vy, weight):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.weight = weight
+
+# Initialize particle list
+num_particles = 3
+particles = [Particle(np.random.random(), np.random.random(), 0.01 * (np.random.random() - 0.5), 0.01 * (np.random.random() - 0.5), np.random.uniform(0.1, 2.0)) for _ in range(num_particles)]
+
+# Initialize Pygame
+pygame.init()
+
+# Set window dimensions
+width, height = 800, 600
+
+# Create a windowed mode window and set display flags
+display = (width, height)
+pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+# Set OpenGL viewport
+glViewport(0, 0, width, height)
+
+# Main simulation loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    # Gravitational Effects
+    G = 0.0005  # Gravitational constant
+
+    for particle1 in particles:
+        for particle2 in particles:
+            if particle1 != particle2:
+                dx = particle2.x - particle1.x
+                dy = particle2.y - particle1.y
+                distance = max(np.sqrt(dx**2 + dy**2), 0.1)
+                force = (G * particle1.weight * particle2.weight) / (distance ** 2)
+                angle = np.arctan2(dy, dx)
+                
+                particle1.vx += force * np.cos(angle) / particle1.weight
+                particle1.vy += force * np.sin(angle) / particle1.weight
+
+    # Update particle positions and velocities
+    for particle in particles:
+        particle.x += particle.vx
+        particle.y += particle.vy
+
+        # Bounce off borders
+        if particle.x <= 0 or particle.x >= 1:
+            particle.vx *= -1
+        if particle.y <= 0 or particle.y >= 1:
+            particle.vy *= -1
+
+    # Draw particles as points using OpenGL
+    glPointSize(3)
+    glBegin(GL_POINTS)
+    for particle in particles:
+        glVertex2f(particle.x, particle.y)
+    glEnd()
+
+    pygame.display.flip()
+    pygame.time.wait(100)
