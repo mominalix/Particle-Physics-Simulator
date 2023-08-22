ade0f57c35e7b6fd6fdf1f7413aa48ddaeffaf75
mominalix <mominalix@gmail.com>
2023-08-22 17:25:18 +0500

    Changes to be committed: 	new file:   Simulator/p_logging/p_logging.py
    Changes not staged for commit:
    	deleted:    Simulator (Beta)/__pycache__/ai_integration.cpython-311.pyc
    	deleted:    Simulator (Beta)/__pycache__/config.cpython-311.pyc
    	deleted:    Simulator (Beta)/__pycache__/particle.cpython-311.pyc
    	deleted:    Simulator (Beta)/__pycache__/physics.cpython-311.pyc
    	deleted:    Simulator (Beta)/ai_integration.py
    	deleted:    Simulator (Beta)/config.py
    	deleted:    Simulator (Beta)/logging/logging.py
    	deleted:    Simulator (Beta)/main.py
    	deleted:    Simulator (Beta)/particle.py
    	deleted:    Simulator (Beta)/physics.py
    	deleted:    Simulator (Beta)/simulation/__pycache__/simulation_cpu.cpython-311.pyc
    	deleted:    Simulator (Beta)/simulation/__pycache__/simulation_cuda.cpython-311.pyc
    	deleted:    Simulator (Beta)/simulation/__pycache__/simulation_opencl.cpython-311.pyc
    	deleted:    Simulator (Beta)/simulation/simulation_cpu.py
    	deleted:    Simulator (Beta)/simulation/simulation_cuda.py
    	deleted:    Simulator (Beta)/simulation/simulation_opencl.py
    	deleted:    Simulator (Beta)/utils.py
    	deleted:    Simulator (Beta)/visualization/visualization.py
    
    Untracked files:
    	Simulator/__pycache__/
    	Simulator/ai_integration.py
    	Simulator/config.py
    	Simulator/main.py
    	Simulator/p_logging/__pycache__/
    	Simulator/particle.py
    	Simulator/particle_simulation_kernal.cl
    	Simulator/physics.py
    	Simulator/requirements.txt
    	Simulator/simulation/
    	Simulator/utils.py
    	Simulator/visualization/

---
+++ Simulator/p_logging/p_logging.py
@@ -0,0 +1,11 @@
+class ParticleLogger:
+    def __init__(self, log_file):
+        self.log_file = log_file
+
+    def log_particle_path(self, particle):
+        # Log the particle's path to the log file
+        pass
+
+    # Implement other logging methods as needed
+from visualization import Visualization
+from p_logging.p_logging import ParticleLogger
