24d60f92f6789cc8e94df54a31d28b6cca8c278e
mominalix <mominalix@gmail.com>
2023-09-22 12:09:02 +0500

    modified:   Simulator/simulation/simulation_cuda.py

--- Simulator/simulation/simulation_cuda.py
+++ Simulator/simulation/simulation_cuda.py
@@ -1,8 +1,73 @@
+import numpy as np
+import pycuda.autoinit
+import pycuda.driver as cuda
+from pycuda.compiler import SourceModule
+
 class CudaSimulation:
     def __init__(self, particles, physics_engine):
         self.particles = particles
         self.physics_engine = physics_engine
 
+        # Define CUDA kernel
+        self.cuda_kernel = """
+        __global__ void simulate_particles(float *x, float *y, float *z, float *vx, float *vy, float *vz, float *weight, int num_particles, float time_step) {
+            int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+            if (idx < num_particles) {
+                // Perform physics calculations here for the particle at index 'idx'
+                // Update the particle's position and velocity based on physics laws
+                // Example:
+                x[idx] += vx[idx] * time_step;
+                y[idx] += vy[idx] * time_step;
+                z[idx] += vz[idx] * time_step;
+            }
+        }
+        """
+
+        # Compile CUDA kernel
+        self.mod = SourceModule(self.cuda_kernel)
+        self.cuda_simulate_particles = self.mod.get_function("simulate_particles")
+
     def simulate(self):
-        # CUDA simulation logic
-        pass
+        num_particles = len(self.particles)
+        particle_size = num_particles * 4  # Assuming each particle has 4 properties (x, y, z, weight)
+
+        # Initialize device memory
+        device_x = cuda.mem_alloc(particle_size)
+        device_y = cuda.mem_alloc(particle_size)
+        device_z = cuda.mem_alloc(particle_size)
+        device_vx = cuda.mem_alloc(particle_size)
+        device_vy = cuda.mem_alloc(particle_size)
+        device_vz = cuda.mem_alloc(particle_size)
+        device_weight = cuda.mem_alloc(particle_size)
+
+        # Copy data from host to device
+        cuda.memcpy_htod(device_x, np.array([particle.x for particle in self.particles], dtype=np.float32))
+        cuda.memcpy_htod(device_y, np.array([particle.y for particle in self.particles], dtype=np.float32))
+        cuda.memcpy_htod(device_z, np.array([particle.z for particle in self.particles], dtype=np.float32))
+        cuda.memcpy_htod(device_vx, np.array([particle.vx for particle in self.particles], dtype=np.float32))
+        cuda.memcpy_htod(device_vy, np.array([particle.vy for particle in self.particles], dtype=np.float32))
+        cuda.memcpy_htod(device_vz, np.array([particle.vz for particle in self.particles], dtype=np.float32))
+        cuda.memcpy_htod(device_weight, np.array([particle.weight for particle in self.particles], dtype=np.float32))
+
+        # Define block and grid sizes
+        block_size = 256  # Adjust as needed
+        grid_size = (num_particles + block_size - 1) // block_size
+
+        time_step = 0.01  # Adjust as needed
+
+        # Execute CUDA kernel
+        self.cuda_simulate_particles(
+            device_x, device_y, device_z, device_vx, device_vy, device_vz, device_weight,
+            np.int32(num_particles), np.float32(time_step),
+            block=(block_size, 1, 1), grid=(grid_size, 1)
+        )
+
+        # Copy results back to host
+        cuda.memcpy_dtoh(np.array([particle.x for particle in self.particles], dtype=np.float32), device_x)
+        cuda.memcpy_dtoh(np.array([particle.y for particle in self.particles], dtype=np.float32), device_y)
+        cuda.memcpy_dtoh(np.array([particle.z for particle in self.particles], dtype=np.float32), device_z)
+        cuda.memcpy_dtoh(np.array([particle.vx for particle in self.particles], dtype=np.float32), device_vx)
+        cuda.memcpy_dtoh(np.array([particle.vy for particle in self.particles], dtype=np.float32), device_vy)
+        cuda.memcpy_dtoh(np.array([particle.vz for particle in self.particles], dtype=np.float32), device_vz)
+        cuda.memcpy_dtoh(np.array([particle.weight for particle in self.particles], dtype=np.float32), device_weight)
